909251930f1c5128ae7d674865c99c19878e869b
Rnaniqw3 <95932621+Rnaniqw3@users.noreply.github.com>
2021-12-10 15:38:00 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 mkdir CCP
 cd CCP 
 echo "Installing..."
-git clone https://github.com/Rnaniqw3/CodingChat/CCProfessional/PyScript/PyScript.git
+git clone https://github.com/Rnaniqw3/CodingChat/CCProfessional/1scriptr/QScript.sh
 cd ..
 echo "Done!"
